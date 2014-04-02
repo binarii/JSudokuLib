690ccb0bdbada24774f393a6324ac1e53b0b0b48
Ryan Zuklie <rzuklie@gmail.com>
2014-04-02 18:05:37 -0400

    Added fisher-yates shuffle

---
+++ src/util/RandUtil.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.util.Random;
+
+public class RandUtil {
+
+	public static void ShuffleArray(int[] array) {
+		Random rnd = new Random();
+		for (int i = array.length - 1; i > 0; i--) {
+			int index = rnd.nextInt(i + 1);
+			// Simple swap
+			int a = array[index];
+			array[index] = array[i];
+			array[i] = a;
+		}
+	}
+}
