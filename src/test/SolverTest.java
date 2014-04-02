4e1db02e734499d18420a06bccae7944d9783b94
Ryan Zuklie <rzuklie@gmail.com>
2014-04-02 11:01:26 -0400

    Added pretty printing

--- src/test/SolverTest.java
+++ src/test/SolverTest.java
@@ -1,40 +1,45 @@
 package test;
 
-import java.io.File;
-
 import board.Board;
 import solve.QuickSolve;
 import util.PuzzleFileReader;
 import util.Timer;
 
 public class SolverTest {
-	private Timer m_timer;
-	
 	private static final String fileName = "puzzles/top1465.txt";
-	
+
 	public static void test() {
+		Timer timer = new Timer();
+		timer.startTimer();
+
 		PuzzleFileReader reader = new PuzzleFileReader(fileName);
 		QuickSolve solver = new QuickSolve();
-		
+
 		int totalCount = 0;
 		int solveCount = 0;
 		double totalTime = 0.0;
-		
-		while(reader.isReady()) {
+		double realTime = 0.0;
+
+		while (reader.isReady()) {
 			Board board = reader.readBoard();
+
 			solver.solve(board);
-			
+
 			totalCount++;
 			solveCount += board.isBoardSolved() ? 1 : 0;
-			totalTime += solver.getSolveTime();			
+			totalTime += solver.getSolveTime();
 		}
-		
+
 		reader.close();
-		
+
+		timer.stopTimer();
+		realTime = timer.getTimeMS();
+
 		System.out.println("Test finished after " + totalTime + "ms");
+		System.out.println("Test real time: " + realTime + "ms");
 		System.out.println("Solved " + solveCount + "/" + totalCount + " puzzles");
 	}
-	
+
 	public static void main(String[] args) {
 		test();
 	}
--- src/util/BoardIO.java
+++ src/util/BoardIO.java
@@ -20,12 +20,46 @@ public class BoardIO {
 		}
 	}
 
-	public static String getBoardString(Board board) {
+	public static String getString(Board board) {
 		StringBuilder builder = new StringBuilder();
 
 		for (int i = 0; i < Board.GRID_SIZE; i++) {
 			char c = Bitmask.toChar(board.getValue(i));
 			builder.append(c);
+		}
+
+		return builder.toString();
+	}
+
+	public static String prettyPrint(Board board) {
+		StringBuilder builder = new StringBuilder();
+
+		for (int row = 0; row < Board.UNIT_SIZE; row++) {
+			// Print the row dividers every 3 rows
+			if (row > 0 && row % Board.BOX_SIZE == 0) {
+				builder.append("------+-------+------\n");
+			}
+
+			for (int col = 0; col < Board.UNIT_SIZE; col++) {
+				// Print the column divider every 3 characters
+				if (col > 0 && col % Board.BOX_SIZE == 0) {
+					builder.append("| ");
+				}
+
+				// Get the proper cell and print character
+				int index = row * Board.UNIT_SIZE + col;
+				char c = Bitmask.toChar(board.getValue(index));
+
+				// If we don't have a value, print a spacer
+				if (c != '0') {
+					builder.append(c);
+				} else {
+					builder.append("-");
+				}
+
+				builder.append(" ");
+			}
+			builder.append("\n");
 		}
 
 		return builder.toString();
